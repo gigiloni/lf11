cdb7f642fb67b91131a0675beb2e50834d5350c0
Georgii Tevzadze <georgii.tevzadze@idmt.fraunhofer.de>
2026-08-26 13:08:20 +0200

    add missing excluded folders in .gitignore; add a Tablet class and relocate drawio stuff to docs folder

---
+++ src/de/bsz/assethub/model/Tablet.java
@@ -0,0 +1,44 @@
+package de.bsz.assethub.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Tablet extends Asset {
+
+    private int storageCapacityGb;
+
+    public Tablet(
+            String inventoryNumber,
+            String description,
+            LocalDate purchaseDate,
+            BigDecimal purchasePrice,
+            String location,
+            int storageCapacityGb) {
+
+        super(
+                inventoryNumber,
+                description,
+                purchaseDate,
+                purchasePrice,
+                location
+        );
+
+        this.storageCapacityGb = storageCapacityGb;
+    }
+
+    public int getStorageCapacityGb() {
+        return storageCapacityGb;
+    }
+
+    public void setStorageCapacityGb(int storageCapacityGb) {
+        this.storageCapacityGb = storageCapacityGb;
+    }
+
+    @Override
+    public String toString() {
+        return "Tablet{"
+                + "asset=" + super.toString()
+                + ", storageCapacityGb=" + storageCapacityGb
+                + '}';
+    }
+}
